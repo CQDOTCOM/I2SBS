deb650389817da59afa388c60611804e1688a187
CQ <chewchongqiang@gmail.com>
2018-12-26 18:03:14 +0800

    updated live chat page

--- js/main.js
+++ js/main.js
@@ -258,47 +258,6 @@
 
   };
 
-	/* slick test */
-	var slickTest = function() {
-
-
-	$('.responsive').slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
-};
-
 
   /* Animations
    * ------------------------------------------------------- */
@@ -413,26 +372,26 @@
     });
   };
 
-	/*owl
-	*/
-	var owlcarousel = function() {
-		$('.owl-carousel').owlCarousel({
-		    loop:true,
-		    margin:10,
-		    nav:true,
-		    responsive:{
-		        0:{
-		            items:1
-		        },
-		        600:{
-		            items:3
-		        },
-		        1000:{
-		            items:5
-		        }
-		    }
-		})
-	}
+  /*owl
+   */
+  var owlcarousel = function() {
+    $('.owl-carousel').owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 3
+        },
+        1000: {
+          items: 5
+        }
+      }
+    })
+  }
 
 
 
@@ -530,8 +489,7 @@
     ssAjaxChimp();
     ssBackToTop();
     ssCardCarousel();
-		slickTest();
-		owlcarousel();
+
 
 
   })();
